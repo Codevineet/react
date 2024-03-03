052af805f33c1ec4c910062d05cc8b020d9f7c16
Vineet Jindal <vineet.jindal.21cse@bmu.edu.in>
2024-03-03 17:20:16 +0530

    Initial commit

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Greet from "./Test"
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hello world, my name is vineet</h1> */}
+      <Greet/>
+    </>
+  )
+}
+
+export default App
---
+++ vitereact/src/Test.jsx
@@ -0,0 +1,9 @@
+function Greet(){
+    return (
+        <>
+        <h2>Hello there!! let's fuck...?</h2>
+        </>
+    )
+}
+
+export default Greet;
